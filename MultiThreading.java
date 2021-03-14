acba0e523edf91f7843029d3b2030bb692a716dd
Mohammad-Mubassir <triplem656@gmail.com>
2021-03-14 15:14:17 +0530

    ..

--- MultiThreading.java
+++ MultiThreading.java
@@ -4,12 +4,21 @@ public class MultiThreading{
         
     Thread thread1 = new MyClass();
     thread1.start();
+    thread1.setName("my thread 1");
+    System.out.println(thread1.getId());
+    System.out.println(thread1.getName());
+    
+
 
     Thread thread2 = new MyClass();
     thread2.start();
+    System.out.println(thread2.getId());
 
     Thread thread3i = new Thread(new MyClass2());
     thread3i.start();
+    System.out.println(thread3i.getId());
+    
+
 
     }
 }
@@ -19,6 +28,16 @@ class MyClass extends Thread{
     @Override
     public void run() {
         System.out.println("Thread created!");
+
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
 }
@@ -30,7 +49,5 @@ class MyClass2 implements Runnable{
         System.out.println("Thread created with interface");
         
     }
-
-
 
 }
